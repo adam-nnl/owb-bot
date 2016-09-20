eab83fcbfffda861832db717d522fff3142070b6
Adam Lara <adam.nnl@gmail.com>
2016-09-19 23:12:58 -0500

    Update empty.skill.js

--- src/empty.skill.js
+++ src/empty.skill.js
@@ -2,5 +2,5 @@
 module.exports = function(skill, info, bot, message) {
   console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
   bot.reply(message, 'I understood this as ' + skill +
-    ', but you haven\'t configured how to make me work yet!');
+    ', but it is not configured to do anything yet! Contact your local code monkey and have them add some functionality to the skill javascript file :computer::monkey:');
 };
