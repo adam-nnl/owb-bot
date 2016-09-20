cc3f44b601145073b359cdec9c289f045d498aac
Adam Lara <adam.nnl@gmail.com>
2016-09-20 13:50:14 -0500

    Update Help.js

--- skills/Help.js
+++ skills/Help.js
@@ -1,3 +1,40 @@
 module.exports = function(skill, info, bot, message) {
+    var builtinPhrases = require('./builtins')
+
+    var customPhrasesText;
+    var customPhrases;
+    try {
+        customPhrasesText = fs.readFileSync(__dirname + '/custom-phrases.json').toString();
+    } catch (err) {
+        throw new Error('Uh oh, Bottie could not find the custom-phrases.json file, did you move it?');
+    }
+    
+    try {
+        customPhrases = JSON.parse(customPhrasesText);
+    } catch (err) {
+        throw new Error('Uh oh, custom-phrases.json was not valid JSON! Fix it, please? :)');
+    }
+
+
+//eachKey(customPhrases, Bottie.Teach);
+//eachKey(builtinPhrases, Bottie.Teach);
+   
     bot.reply(message,'These are my current commands: ' );
+    
+    bot.reply(message,'Built-in commands: ' );
+    for (var key in p) {
+        if (p.hasOwnProperty(key)) {
+            bot.reply(message,'These are my current commands: ' );
+            //console.log(key + " -> " + p[key]);
+        }
+    }
+    
+    bot.reply(message,'Custom-added commands: ' );
+    for (var key in p) {
+        if (p.hasOwnProperty(key)) {
+            bot.reply(message,'These are my current commands: ' );
+            //console.log(key + " -> " + p[key]);
+        }
+    }
+    
 };
