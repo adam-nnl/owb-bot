8e3f558c5a8643ec1a7c7c940194685b3cda1404
Adam Lara <adam.nnl@gmail.com>
2016-09-19 23:18:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ Bottie.Brain.think();
 console.log('Bottie finished learning, time to listen...');
 Bottie.Ears
   .listen()
-  .hear('TRAINING TIME!!!', function(speech, message) {
+  .hear('LEARN-SKILL', function(speech, message) {
     console.log('Delegating to on-the-fly training module...');
     Train(Bottie.Brain, speech, message);
   })
