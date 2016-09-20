10926e5856aeda9faee96ae59e9c237d7283796d
Adam Lara <adam.nnl@gmail.com>
2016-09-20 15:45:31 -0500

    Update Backup.js

--- skills/Backup.js
+++ skills/Backup.js
@@ -1,4 +1,10 @@
 module.exports = function(skill, info, bot, message) {
-  var simpleGit = require('simple-git');
   bot.reply(message,'This will be a git push backup to remote master' );
+    require('simple-git')()
+         .add('./*')
+         .commit("bot commit!")
+         .addRemote('origin', 'https://github.com/adam-nnl/owb-bot')
+         .push(['-u', 'origin', 'master'], function () {
+            // done. 
+         });
 };
