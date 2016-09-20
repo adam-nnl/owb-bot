ddc71cd7aa36966876d63d5dac8c4c43c9185646
Adam Lara <adam.nnl@gmail.com>
2016-09-20 13:36:04 -0500

    Update Info.js

--- skills/Info.js
+++ skills/Info.js
@@ -21,7 +21,7 @@ module.exports = function(skill, info, bot, message) {
     bot.reply(message, 'Total memory: ' + totalmem + ' bytes');
     for(var i = 0, len = cpus.length; i < len; i++) {
 	var cpu = cpus[i], total = 0, processTotal = 0, strPercent = '';
-	bot.reply(message, 'CPU core: ' + i);
+	bot.reply(message, 'CPU cores: ' + cpus.length);
 	bot.reply(message, 'CPU model: ' + cpu.model);
 	bot.reply(message, 'CPU speed: ' + cpu.speed +'MHz');
 	//console.log("CPU core:", i);
