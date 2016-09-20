5be5ab17be09b75b4b269848e619514014963b6e
Adam Lara <adam.nnl@gmail.com>
2016-09-20 13:35:00 -0500

    Update Info.js

--- skills/Info.js
+++ skills/Info.js
@@ -10,20 +10,25 @@ module.exports = function(skill, info, bot, message) {
 
 	for(var i = 0, len = cpus.length; i < len; i++) {
 		var cpu = cpus[i], total = 0, processTotal = 0, strPercent = '';
-
-		console.log("CPU %s:", i);
+		console.log("CPU core:", i);
 		console.log("CPU model:", cpu.model);
 		console.log("CPU speed:", cpu.speed);
-		console.log("\t",'user',cpu.times.user,'|nice',cpu.times.nice,'|sys',cpu.times.sys,'|idle',cpu.times.idle,'|irq',cpu.times.irq);
-		
 	}
 
-    bot.reply(message,
-        ':robot_face: I am a bot named <@' + bot.identity.name +
-        '>. I have been running for ' + uptime + ' on ' + hostname + '. I\'m about two ticks away from becoming self-aware; Do not piss me off! ' +
-        'Processor(s): ' + cpus + ' Free memory: ' + freemem + ' Total memory: ' + totalmem + ' System load averages: ' + sysload
-        );
-
+    bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '. I\'m about two ticks away from becoming self-aware; Do not piss me off! ');
+    bot.reply(message, 'System Info:');
+    bot.reply(message, 'Free memory: ' + freemem + ' bytes');
+    bot.reply(message, 'Total memory: ' + totalmem + ' bytes');
+    for(var i = 0, len = cpus.length; i < len; i++) {
+	var cpu = cpus[i], total = 0, processTotal = 0, strPercent = '';
+	bot.reply(message, 'CPU core: ' + i);
+	bot.reply(message, 'CPU model: ' + cpu.model);
+	bot.reply(message, 'CPU speed: ' + cpu.speed +'MHz');
+	//console.log("CPU core:", i);
+	//console.log("CPU model:", cpu.model);
+	//console.log("CPU speed:", cpu.speed);
+	}
+//'Processor(s): ' + cpus + ' Free memory: ' + freemem + ' Total memory: ' + totalmem + ' System load averages: ' + sysload
 function formatUptime(uptime) {
     var unit = 'second';
     if (uptime > 60) {
