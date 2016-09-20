087cb2553a5e7356a92598222ed35cd374d0d0ee
Adam Lara <adam.nnl@gmail.com>
2016-09-20 13:57:56 -0500

    Update Help.js

--- skills/Help.js
+++ skills/Help.js
@@ -4,7 +4,7 @@ module.exports = function(skill, info, bot, message) {
 
     var customPhrasesText;
     var customPhrases;
-    try {
+    /*try {
         customPhrasesText = fs.readFileSync('../custom-phrases.json').toString();
     } catch (err) {
         throw new Error('Uh oh, Bottie could not find the custom-phrases.json file, did you move it?');
@@ -14,7 +14,7 @@ module.exports = function(skill, info, bot, message) {
         customPhrases = JSON.parse(customPhrasesText);
     } catch (err) {
         throw new Error('Uh oh, custom-phrases.json was not valid JSON! Fix it, please? :)');
-    }
+    }*/
 
 
 //eachKey(customPhrases, Bottie.Teach);
@@ -23,19 +23,19 @@ module.exports = function(skill, info, bot, message) {
     bot.reply(message,'These are my current commands: ' );
     
     bot.reply(message,'Built-in commands: ' );
-    for (var key in p) {
-        if (p.hasOwnProperty(key)) {
+    for (var key in builtinPhrases) {
+        if (builtinPhrases.hasOwnProperty(key)) {
             bot.reply(message,'These are my current commands: ' );
-            //console.log(key + " -> " + p[key]);
+            console.log(key + " -> " + builtinPhrases[key]);
         }
     }
     
-    bot.reply(message,'Custom-added commands: ' );
-    for (var key in p) {
-        if (p.hasOwnProperty(key)) {
-            bot.reply(message,'These are my current commands: ' );
-            //console.log(key + " -> " + p[key]);
-        }
-    }
+    //bot.reply(message,'Custom-added commands: ' );
+    //for (var key in p) {
+    //    if (p.hasOwnProperty(key)) {
+    //        bot.reply(message,'These are my current commands: ' );
+    //        //console.log(key + " -> " + p[key]);
+    //    }
+    //}
     
 };
