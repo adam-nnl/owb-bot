c8a093c55a1d50f7850d9da50125195d2289ce44
Adam Lara <adam.nnl@gmail.com>
2016-09-20 13:23:13 -0500

    Update Info.js

--- skills/Info.js
+++ skills/Info.js
@@ -8,6 +8,28 @@ module.exports = function(skill, info, bot, message) {
     var freemem = os.freemem();
     var totalmem = os.totalmem();
 
+	for(var i = 0, len = cpus.length; i < len; i++) {
+		var cpu = cpus[i], total = 0, processTotal = 0, strPercent = '';
+
+		console.log("CPU %s:", i);
+		console.log("\t",'user',cpu.times.user,'|nice',cpu.times.nice,'|sys',cpu.times.sys,'|idle',cpu.times.idle,'|irq',cpu.times.irq);
+		
+		for(type in cpu.times){
+			total += cpu.times[type];
+		}
+
+		for(type in cpu.times){
+			var percent = Math.round(100 * cpu.times[type] / total);
+			strPercent += type + ' ' + percent + '%|';
+			if(type != 'idle'){
+				processTotal += percent;	
+			}
+		}
+		console.log("\t",strPercent)
+		console.log("\t",'T0TAL PROCESSOR: ',total);
+		console.log("\t",'TOTAL: ',processTotal);
+	}
+
     bot.reply(message,
         ':robot_face: I am a bot named <@' + bot.identity.name +
         '>. I have been running for ' + uptime + ' on ' + hostname + '. I\'m about two ticks away from becoming self-aware; Do not piss me off! ' +
