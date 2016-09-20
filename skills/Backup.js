970863d851f8c5f748e8362b4cbf2754db5df6c8
Adam Lara <adam.nnl@gmail.com>
2016-09-20 13:40:22 -0500

    Update Backup.js

--- skills/Backup.js
+++ skills/Backup.js
@@ -1,4 +1,4 @@
 module.exports = function(skill, info, bot, message) {
-  var simpleGit = require('simple-git')( workingDirPath );
+  var simpleGit = require('simple-git');
   bot.reply(message,'This will be a git push backup to remote master' );
 };
