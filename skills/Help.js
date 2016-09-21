34b44244be612c1a8a12eea45fc127493201d0a8
Adam Lara <adam.nnl@gmail.com>
2016-09-20 22:22:32 -0500

    Update Help.js

--- skills/Help.js
+++ skills/Help.js
@@ -3,20 +3,22 @@ module.exports = function(skill, info, bot, message) {
     var customPhrases = require('../custom-phrases.json');
    
     //bot.reply(message,'These are my current commands: ' );
-    
+    bot.reply(message,'Say `!TRAIN` to begin a custom skill training session.' );
     bot.reply(message,'These are my current built-in commands: ' );
     for (var key in builtinPhrases) {
         if (builtinPhrases.hasOwnProperty(key)) {
             bot.reply(message, '*Command:* ' + key);
             bot.reply(message, '*Key words/phrases:* ' + builtinPhrases[key]);
+            bot.reply(message, ' ');
         }
     }
     
     bot.reply(message,'These are my current custom-added commands: ' );
-    for (var key in customPhrases) {
-        if (customPhrases.hasOwnProperty(key)) {
-            bot.reply(message, '*Command:* ' + key);
-            bot.reply(message, '*Key words/phrases:* ' + customPhrases[key]);
+    for (var key2 in customPhrases) {
+        if (customPhrases.hasOwnProperty(key2)) {
+            bot.reply(message, '*Command:* ' + key2);
+            bot.reply(message, '*Key words/phrases:* ' + customPhrases[key2]);
+            bot.reply(message, ' ');
         }
     }
     
