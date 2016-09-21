baedd06f40001c3c259d1155934d29120dbf8564
Adam Lara <adam.nnl@gmail.com>
2016-09-20 20:58:42 -0500

    Update FuckOff.js

--- skills/FuckOff.js
+++ skills/FuckOff.js
@@ -1,4 +1,25 @@
 module.exports = function(skill, info, bot, message) {
-  bot.reply(message, 'http://foaas.com/everyone/' + bot.identity.name);
+    bot.api.users.info({user: message.user}, (error, response) => {
+    var fuckArray = [
+    'off',
+    'ing',
+    'you',
+    'donut',
+    'shakespeare',
+    'linus',
+    'king',
+    'chainsaw',
+    'outside',
+    'madison',
+    'yoda',
+    'nugget',
+    'shutup',
+    'think'
+    ];
+    var randomFuck = Math.floor(Math.random()*fuckArray.length);    
+        let {name, real_name} = response.user;
+        bot.reply(message, 'http://foaas.com/' + fuckArray[randomFuck] + '/' + name + '/' + bot.identity.name);
+    })  
+  //bot.reply(message, 'http://foaas.com/everyone/' + bot.identity.name);
   //bot.reply(message, 'I understood this as ' + skill + ', but you haven\'t configured how to make me work yet! Tell your local code monkey to fill in the code module for ' + skill);
 };
