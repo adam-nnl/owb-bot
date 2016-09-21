0bbc2f3531f8d8398d8acc78bf464c3632531b69
Adam Lara <adam.nnl@gmail.com>
2016-09-21 12:31:10 -0500

    Update Backup.js

--- skills/Backup.js
+++ skills/Backup.js
@@ -1,16 +1,17 @@
 module.exports = function(skill, info, bot, message) {
     bot.api.users.info({user: message.user}, (error, response) => {
     let {name, real_name} = response.user;
-    bot.reply(message, 'http://foaas.com/' + fuckArray[randomFuck] + '/' + name + '/' + bot.identity.name);
-    }) 
-  
-  bot.reply(message,'This will be a git push backup to remote master' );
+
+  bot.reply(message,'This will be a git commit and push from local/master to remote/master' );
     require('simple-git')()
          .add('./*')
-         .commit("bot commit!")
+         .commit("bot commit by " + name )
          //.addRemote('origin', 'https://github.com/adam-nnl/owb-bot')
          .push(['-u', 'origin', 'master'], function () {
             // done. 
          });
-  bot.reply(message,'Git commit and push complete!' );
+  bot.reply(message,'Git commit and push complete! :D' );
+  
+}) 
+  
 };
