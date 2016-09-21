c8db97f5246b0613e42d0a43a7be67031d00bcea
Adam Lara <adam.nnl@gmail.com>
2016-09-21 10:43:09 -0500

    Update Ping.js

--- skills/Ping.js
+++ skills/Ping.js
@@ -1,6 +1,4 @@
 
 module.exports = function(skill, info, bot, message) {
-  console.log('INVOCATION OF NON-CONFIGURED SKILL: ' + skill);
-  bot.reply(message, 'I understood this as *' + skill +
-    '*, but it is not configured to do anything yet! Contact your local code monkey :computer::monkey: and ask them to add some functionality to the javascript file for this skill.');
+  bot.reply(message, 'PONG!');
 };
