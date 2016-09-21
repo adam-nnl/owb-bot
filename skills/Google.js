09653bf2bb2fcdef01568dd3ccbbdf69a6f21f36
Adam Lara <adam.nnl@gmail.com>
2016-09-20 21:11:11 -0500

    Update Google.js

--- skills/Google.js
+++ skills/Google.js
@@ -1,10 +1,13 @@
 module.exports = function(skill, info, bot, message) {
+    var regex = /[0-9]+/g;
+var result = regex[Symbol.match](message.text);
+console.log(result);  // ["2016", "01", "02"]
     
     //'google (.*)'
-    var regex = /google (.*)/i;
-    var query = message.text.match[regex];
+    //var regex = /google (.*)/i;
+    //var query = message.text.match[regex];
     
     //var query = message.text.replace(skill,'');
-    var search_str = require('querystring').escape(query[1]);
+    //var search_str = require('querystring').escape(query[1]);
     bot.reply(message,':mag: Let me Google that for you!- ' + 'https://www.google.com/?#q=' + search_str );
 };
