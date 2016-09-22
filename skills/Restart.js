1c6c0492fd86e064018dbff433f3eff81e416f30
Adam Lara <adam.nnl@gmail.com>
2016-09-21 21:32:33 -0500

    Update Restart.js

--- skills/Restart.js
+++ skills/Restart.js
@@ -34,7 +34,7 @@ module.exports = function(skill, info, bot, message) {
     ];
     var randomBye = Math.floor(Math.random()*byeArray.length);    
     bot.reply(message, byeArray[randomBye]);
-    wait(1000);
+    wait(5000);
     process.exit(0);
     
 function wait(ms){
