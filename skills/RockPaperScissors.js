8682674a9dce7447271b419e9f9e826968e3e497
Adam Lara <adam.nnl@gmail.com>
2016-09-21 22:55:28 -0500

    Update RockPaperScissors.js

--- skills/RockPaperScissors.js
+++ skills/RockPaperScissors.js
@@ -1,5 +1,9 @@
 module.exports = function(skill, info, bot, message, db) {
   var gameEngine = require('rock-paper-spock');
+  
+    bot.api.users.info({user: message.user}, (error, response) => {
+        bot.reply(message, 'Rock Paper Scissors (lizard, spock, wizard, batman, glock?)');
+    })  
 
   var instance = new gameEngine(gameEngine.DefaultRules);
   instance.addPlayer({id: 1, sign: "spock"});
@@ -8,5 +12,5 @@ module.exports = function(skill, info, bot, message, db) {
   instance.addPlayer(4, "rock").addPlayer(5, "paper");
   instance.play();
   console.log(instance.winner);
-  bot.reply(message, 'test successful. maybe code something more useful next time?');
+  //bot.reply(message, 'test successful. maybe code something more useful next time?');
 };
