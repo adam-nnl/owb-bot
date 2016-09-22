721ca3a916b91c60bdab522c6ec27bea39f2ddc7
Adam Lara <adam.nnl@gmail.com>
2016-09-21 22:33:00 -0500

    Update Jokes.js

--- skills/Jokes.js
+++ skills/Jokes.js
@@ -1,4 +1,4 @@
-module.exports = function(skill, info, bot, message) {
+module.exports = function(skill, info, bot, message, db) {
     var jokesArray = require('./jokes.json');
     var randomJoke = Math.floor(Math.random()*jokesArray.length);   
     bot.reply(message, jokesArray[randomJoke]);
