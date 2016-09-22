217b6e8395eda0ff2d95bd16fea9a36672df0a39
Adam Lara <adam.nnl@gmail.com>
2016-09-22 14:54:46 -0500

    Update SecretHug.js

--- skills/SecretHug.js
+++ skills/SecretHug.js
@@ -1,8 +1,15 @@
 module.exports = function(skill, info, bot, message, db) {
     bot.api.users.info({user: message.user}, (error, response) => {
-        let {name, real_name} = response.user;
-        bot.reply(message, 'under development');
+    let {name, real_name} = response.user;
+    bot.reply(message, 'under development');
+    var userData = message.text.match(/<@([A-Z0–9]{9})>/); // parse the text for user's 9 character id
+
+  if (userData) {
+    // if there is a user named send ze hugs!
+  } else {
+    bot.reply(message, 'You didn\'t name anyone…were you the one that needed a hug ' + name + '?');
+  }
+        
     })  
-  //bot.reply(message, 'http://foaas.com/everyone/' + bot.identity.name);
-  //bot.reply(message, 'I understood this as ' + skill + ', but you haven\'t configured how to make me work yet! Tell your local code monkey to fill in the code module for ' + skill);
+
 };
