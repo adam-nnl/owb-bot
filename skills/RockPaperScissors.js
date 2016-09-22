d10ec70a72dcb9b3f0e7312eae2964b15ed76610
Adam Lara <adam.nnl@gmail.com>
2016-09-21 20:24:01 -0500

    Create RockPaperScissors.js

---
+++ skills/RockPaperScissors.js
@@ -0,0 +1,12 @@
+module.exports = function(skill, info, bot, message) {
+  var gameEngine = require('rock-paper-spock');
+
+  var instance = new gameEngine(gameEngine.DefaultRules);
+  instance.addPlayer({id: 1, sign: "spock"});
+  instance.addPlayer({id: 2, sign: "rock"});
+  instance.addPlayer({id: 3, sign: "spock"});
+  instance.addPlayer(4, "rock").addPlayer(5, "paper");
+  instance.play();
+  console.log(instance.winner);
+  bot.reply(message, 'test successful. maybe code something more useful next time?');
+};
