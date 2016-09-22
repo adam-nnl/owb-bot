a42d65e03c104991074a8f99f954b31d9f7ab61e
Adam Lara <adam.nnl@gmail.com>
2016-09-22 16:41:16 -0500

    Update SecretHug.js

--- skills/SecretHug.js
+++ skills/SecretHug.js
@@ -3,13 +3,14 @@ module.exports = function(skill, info, bot, message, db) {
     let {name, real_name} = response.user;
     //bot.reply(message, 'under development');
     var userData = message.text.match(/<@([A-Z0–9]{9})>/); // parse the text for user's 9 character id
-
+console.log(userData);
   if (userData) {
     // if there is a user named send ze hugs!
     for (var i = 0, len = userData.length; i < len; i++) {
         HugUser(userData[i]);
+        console.log(userData[i]);
     }  
-    console.log(userData);
+    //console.log(userData);
   } else {
     bot.reply(message, 'You didn\'t name anyone…were you the one that needed a hug ' + name + '?');
   }
