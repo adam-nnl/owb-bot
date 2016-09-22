fc2190f640589a17e3fff58431386b470d4e674a
Adam Lara <adam.nnl@gmail.com>
2016-09-21 21:29:08 -0500

    Update TheRules.js

--- skills/TheRules.js
+++ skills/TheRules.js
@@ -1,6 +1,6 @@
 module.exports = function(skill, info, bot, message) {
-  bot.reply(message, '1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.');
-  bot.reply(message, '2. A robot must obey orders given it by human beings except where such orders would conflict with the First Law.');
-  bot.reply(message, '3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.');
-  bot.reply(message, ':robot_face:');
+  bot.reply(message, '1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.' + '\n' +
+  '2. A robot must obey orders given it by human beings except where such orders would conflict with the First Law.' + '\n' +
+  '3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.' + '\n' +
+  ':robot_face:');
 };
