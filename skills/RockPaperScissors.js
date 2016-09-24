14f522d8d318449135e384c9aaac4201c7ad3be1
Adam Lara <adam.nnl@gmail.com>
2016-09-24 00:31:07 -0500

    Update RockPaperScissors.js

--- skills/RockPaperScissors.js
+++ skills/RockPaperScissors.js
@@ -54,10 +54,8 @@ function FU(user) {
     //})  
 
   var instance = new gameEngine(gameEngine.DefaultRules);
-  instance.addPlayer({id: 1, sign: "spock"});
+  instance.addPlayer({id: 1, sign: "paper"});
   instance.addPlayer({id: 2, sign: "rock"});
-  instance.addPlayer({id: 3, sign: "spock"});
-  instance.addPlayer(4, "rock").addPlayer(5, "paper");
   instance.play();
   console.log(instance.winner);
   //note players that won-lost, add support for W-L records
