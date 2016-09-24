630e99b1ce7554a9436f33473de35dd170b095fe
Adam Lara <adam.nnl@gmail.com>
2016-09-23 23:09:57 -0500

    Update RockPaperScissors.js

--- skills/RockPaperScissors.js
+++ skills/RockPaperScissors.js
@@ -22,10 +22,8 @@ module.exports = function(skill, info, bot, message, db) {
 
 function getSlackName(userID) {
     bot.api.users.info({user: userID}, (error, response) => {
-    console.log(response.user.real_name);
-    var slackResp = JSON.parse(response);
-    console.log('slackname: ' + slackResp.user.real_name);
-    return slackResp.user.real_name;
+    console.log('slackname: ' + response.user.real_name);
+    return response.user.real_name;
     })
 
 }
