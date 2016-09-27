32520f60e3b86eec7a4e40e45dc7f65879f08bed
Adam Lara <adam.nnl@gmail.com>
2016-09-27 14:58:59 -0500

    Update ears.js

--- src/ears.js
+++ src/ears.js
@@ -20,7 +20,22 @@ Ears.prototype.listen = function() {
   console.log('TOKEN: ' + this.token);
   this.bot = Bot.spawn({
     token: this.token
-  }).startRTM();
+  }).startRTM(){
+    if (err) {
+        throw new Error(err);
+    }
+
+    // @ https://api.slack.com/methods/users.list
+    bot.api.users.list({}, function (err, response) {
+        if (response.hasOwnProperty('members') && response.ok) {
+            var total = response.members.length;
+            for (var i = 0; i < total; i++) {
+                var member = response.members[i];
+                var id = db.saveSync({name: member.name, id: member.id});
+                //fullTeamList.push({name: member.name, id: member.id});
+            }
+        }
+    });
   return this;
 }
 
